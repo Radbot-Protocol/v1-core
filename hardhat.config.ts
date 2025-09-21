a2ddd9bd33f84904d0a7722f0963e3428bb776e1
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-21 03:54:32 -0700

    pre-commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,39 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatContractSizer from "@solidstate/hardhat-contract-sizer";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatContractSizer],
+
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    strict: true,
+  },
   solidity: {
     profiles: {
       default: {
         version: "0.8.28",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+          evmVersion: "istanbul",
+          metadata: {
+            bytecodeHash: "none",
+          },
+        },
       },
       production: {
         version: "0.8.28",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 800,
           },
         },
       },
